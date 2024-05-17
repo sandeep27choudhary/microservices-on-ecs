89c239a574d15d73e13d371c553832c3a86a9ca8
Sandeep Choudhary <csandeep497@gmail.com>
2024-05-17 12:02:49 +0530

    corrected log config

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -23,14 +23,14 @@ resource "aws_ecs_task_definition" "ecs_task" {
       containerPort     = var.ecs_task.container_image_port
     }]
 
-    logConfiguration = [{
-      logDriver = awslogs,
+    logConfiguration = {
+      logDriver = "awslogs"
       options = {
-        awslogs-group = "/fargate/service/${var.ecs_task.family}"
-        awslogs-region = ap-south-1
-        awslogs-stream-prefix = var.ecs_task.family
+        "awslogs-group" = "/fargate/service/${var.ecs_task.family}"
+        "awslogs-region" = "ap-south-1"
+        "awslogs-stream-prefix" = var.ecs_task.family
       }
-    }]
+    }
 
   }])
   cpu                 = var.ecs_task.cpu
