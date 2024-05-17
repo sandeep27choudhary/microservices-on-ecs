142d0728b4c9a8d12488e0b3f6ba761d17146661
Sandeep Choudhary <csandeep497@gmail.com>
2024-05-17 12:50:03 +0530

    sg for inventory

--- terraform/inventory/main.tf
+++ terraform/inventory/main.tf
@@ -41,7 +41,7 @@ resource "aws_ecs_service" "ecs_service" {
     assign_public_ip = false
 
     security_groups = [
-      aws_security_group.binventory_sg.id,
+      aws_security_group.inventory_sg.id,
     ]
 
     subnets = var.ecs_service.private_subnets
